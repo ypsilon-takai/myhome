0cbb31fa6f9c32972a189604af9e3599dc99621c
Ypsilon.takai <ypsilon.takai+github@gmail.com>
2014-08-20 22:41:10 +0900

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -53,9 +53,16 @@
 
 ;; from new startar kit
 (ido-mode t)
-(setq ido-enable-flex-matching t)
+(ido-ubiquitous-mode)
 
-(setq ido-everywhere t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
